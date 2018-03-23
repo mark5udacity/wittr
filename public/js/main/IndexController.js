2fbf3b081fb8a41e21b4b5885af8b172fdc55efd
Mark 5 <markvudacity@gmail.com>
2018-03-23 16:53:29 -0400

    First attempt at notifying users that a new update is ready (ServiceWorker installed).

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -16,22 +16,29 @@ IndexController.prototype._registerServiceWorker = function() {
 
   var indexController = this;
 
+  function installedStateChange() {
+    if (this.state === 'installed') {
+      indexController._updateReady();
+    }
+  }
+
   navigator.serviceWorker.register('/sw.js').then(function(reg) {
-    // TODO: if there's no controller, this page wasn't loaded
-    // via a service worker, so they're looking at the latest version.
-    // In that case, exit early
+    if (!navigator.serviceWorker.controller) {
+      console.debug('First time here, so no SW to worry about');
+      return;
+    }
 
-    // TODO: if there's an updated worker already waiting, call
-    // indexController._updateReady()
+    if (reg.waiting) {
+      indexController._updateReady();
+      return;
+    }
 
-    // TODO: if there's an updated worker installing, track its
-    // progress. If it becomes "installed", call
-    // indexController._updateReady()
+    if (reg.installing) {
+      reg.installing.addEventListener('statechange', installedStateChange);
+      return;
+    }
 
-    // TODO: otherwise, listen for new installing workers arriving.
-    // If one arrives, track its progress.
-    // If it becomes "installed", call
-    // indexController._updateReady()
+    reg.addEventListener('updateFound', () => reg.installing.addEventListener('statechange'), installedStateChange);
   });
 };
 
