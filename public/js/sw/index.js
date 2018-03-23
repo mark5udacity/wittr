834b035cf2011b8565a03c52d7782b33b2d7772f
Mark 5 <markvudacity@gmail.com>
2018-03-23 15:33:45 -0400

    Return from cache, else fetch.  Look ma, just one line!  (Thanks ES6)

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,19 +1,20 @@
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('wittr-static-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-      ]);
-    })
-  );
+    caches.open('wittr-static-v1')
+        .then(cache =>
+            cache.addAll([
+            '/',
+            'js/main.js',
+            'css/main.css',
+            'imgs/icon.png',
+            'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+            'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+            ])
+        )
+      );
 });
 
-self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request)
+      .then(response => response ? response : fetch(event.request)));
 });
