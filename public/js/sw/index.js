0c0f745384deb72a9ed5b5056577cbb9813aa83e
Mark 5 <markvudacity@gmail.com>
2018-03-23 13:50:21 -0400

    Hijacked service request with correct html header and such.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,10 @@
 self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
+	// containing an element with.
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+      new Response('Hello, <b class="a-winner-is-me">World!</b>', {
+        headers: {'Content-Type': 'text/html; charset=utf-8'}
+      })
+  );
 });
