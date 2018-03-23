b49323f0b07a542b49be6628d0fed94d6f88b586
Mark 5 <markvudacity@gmail.com>
2018-03-23 17:30:16 -0400

    First attempt at serving up skeleton (and ES6ifying).

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,49 +1,39 @@
 var staticCacheName = 'wittr-static-v3';
 
-self.addEventListener('install', function(event) {
-  // TODO: cache /skeleton rather than the root page
-
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/',
-        'js/main.js',
-        'css/main.css',
-        'imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
-      ]);
-    })
+    caches.open(staticCacheName).then(cache => cache.addAll([
+      '/skeleton',
+      'js/main.js',
+      'css/main.css',
+      'imgs/icon.png',
+      'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+      'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+    ]))
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('wittr-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
+    caches.keys().then(cacheNames => Promise.all(
+        cacheNames.filter(cacheName => cacheName.startsWith('wittr-') && cacheName != staticCacheName)
+            .map(cacheName => caches.delete(cacheName))
+    ))
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  // TODO: respond to requests for the root page with
-  // the page skeleton from the cache
+self.addEventListener('fetch', event => {
+  let matched;
+  if (event.request.url === '/') {
+    matched = caches.match('/skeleton');
+  } else {
+    matched = caches.match(event.request);
+  }
 
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(matched.then(response => response || fetch(event.request)));
 });
 
-self.addEventListener('message', function(event) {
+self.addEventListener('message', event => {
   if (event.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
